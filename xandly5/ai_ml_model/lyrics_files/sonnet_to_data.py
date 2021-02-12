f8c0ac52ab32d86a41172b1a638f07d00a2fd1ed
Andre Oporto <andreoporto@gmail.com>
2021-02-12 12:28:02 -0500

    use regex instead of line length for sonnet number parsing

--- xandly5/ai_ml_model/lyrics_files/sonnet_to_data.py
+++ xandly5/ai_ml_model/lyrics_files/sonnet_to_data.py
@@ -7,11 +7,15 @@ From fairest creatures we desire increase,	1	1
 That thereby beauty's rose might never die,	1	2
 """
 
+import re
+
 
 def main():
 
     sonnet_number: int = 0
     line_number: int = 0
+    # pattern for sonnet number as roman numeral; only goes up to 154 so no need for M as in MCMLXXXIV
+    roman_num_regexp = re.compile('^[CLXVI]*$')
 
     with open('shakespeare-sonnets.txt', 'r') as sonnets_file:
         with open('shakespeare-sonnets-data.txt', 'w') as sonnets_data:
@@ -25,12 +29,10 @@ def main():
                 if len(clean_line) == 0:
                     # skip blank line
                     continue
-
-                elif len(clean_line) < 10:
-                    # sonnet number indicator; increment sonnet number and increase line count
+                elif roman_num_regexp.search(clean_line):
+                    # sonnet number indicator; increment sonnet number and RESET line count
                     sonnet_number += 1
                     line_number = 0
-
                 else:
                     line_number += 1
                     sonnets_data.write(f'{clean_line}\t{sonnet_number}\t{line_number}\n')
