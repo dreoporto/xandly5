fe818124a1174dd5878efeab288d0f013896a06a
Andre Oporto <andreoporto@gmail.com>
2021-02-19 08:57:47 -0500

    convert poe poem model to a class

--- xandly5/ai_ml_model/poe_poem_model.py
+++ xandly5/ai_ml_model/poe_poem_model.py
@@ -1,6 +1,6 @@
-import os
 import json
 import tensorflow as tf
+
 from tensorflow import keras
 from sklearn.model_selection import train_test_split
 
@@ -15,82 +15,96 @@ def tensorflow_diagnostics():
     print('keras version:', keras.__version__)
 
 
-def get_model(total_words: int, max_sequence_length: int,
-              output_dimensions: int, lstm_units: int) -> keras.Sequential:
-    model = keras.Sequential([
-        keras.layers.Embedding(total_words, output_dimensions, input_length=max_sequence_length - 1),
-        keras.layers.Bidirectional(keras.layers.LSTM(lstm_units)),
-        keras.layers.Dense(total_words, activation='softmax')
-    ])
+class PoePoemModel:
 
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    def __init__(self, config_file):
+        with open(config_file) as json_file:
+            self.config = json.load(json_file)
 
-    model.summary()
+    @staticmethod
+    def _get_model(total_words: int, max_sequence_length: int,
+                   output_dimensions: int, lstm_units: int) -> keras.Sequential:
 
-    return model
+        model = keras.Sequential([
+            keras.layers.Embedding(total_words, output_dimensions, input_length=max_sequence_length - 1),
+            keras.layers.Bidirectional(keras.layers.LSTM(lstm_units)),
+            keras.layers.Dense(total_words, activation='softmax')
+        ])
+
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+        model.summary()
+
+        return model
+
+    def _generate_lyrics(self, model: keras.Sequential, catalog: Catalog):
+
+        lyrics_text = catalog.generate_lyrics_text(
+            model,
+            seed_text=self.config['seed_text'],
+            word_count=self.config['words_to_generate']
+        )
+
+        lyrics = LyricsFormatter.format_lyrics(lyrics_text, self.config['word_group_count'])
+        print(lyrics)
+
+        with open(self.config['saved_lyrics_path'], 'w') as lyrics_file:
+            lyrics_file.write(lyrics)
+            lyrics_file.close()
+
+    def train_model(self):
+
+        catalog = Catalog()
+        catalog.add_file_to_catalog(self.config['lyrics_file_path'])
+        catalog.tokenize_catalog()
+
+        x_train, x_valid, y_train, y_valid = train_test_split(
+            catalog.features, catalog.labels,
+            test_size=self.config['hp_test_size'],
+            random_state=self.config['random_state']
+        )
+
+        model = self._get_model(
+            max_sequence_length=catalog.max_sequence_length,
+            total_words=catalog.total_words,
+            output_dimensions=self.config['hp_output_dimensions'],
+            lstm_units=self.config['hp_lstm_units']
+        )
+
+        early_stopping = keras.callbacks.EarlyStopping(
+            monitor='val_loss',
+            patience=self.config['hp_patience'],
+            min_delta=self.config['hp_min_delta'],
+            mode='min'
+        )
+
+        stopwatch = Stopwatch()
+        stopwatch.start()
+
+        history = model.fit(
+            catalog.features,
+            catalog.labels,
+            validation_data=(x_valid, y_valid),
+            epochs=self.config['hp_epochs'],
+            verbose=1,
+            callbacks=[early_stopping]
+        )
+
+        stopwatch.stop()
+
+        model.save(self.config['saved_model_path'])
+
+        if self.config['is_interactive']:
+            pch.show_history_chart(history, 'accuracy')
+            pch.show_history_chart(history, 'loss')
+
+        self._generate_lyrics(model, catalog)
 
 
 def main():
     tensorflow_diagnostics()
-
-    with open('poe_poem_config.json') as json_file:
-        config = json.load(json_file)
-
-    catalog = Catalog()
-    catalog.add_file_to_catalog(config['lyrics_file_path'])
-    catalog.tokenize_catalog()
-
-    x_train, x_valid, y_train, y_valid = train_test_split(
-        catalog.features, catalog.labels,
-        test_size=config['hp_test_size'],
-        random_state=config['random_state']
-    )
-
-    model = get_model(
-        max_sequence_length=catalog.max_sequence_length,
-        total_words=catalog.total_words,
-        output_dimensions=config['hp_output_dimensions'],
-        lstm_units=config['hp_lstm_units']
-    )
-
-    early_stopping = keras.callbacks.EarlyStopping(
-        monitor='val_loss',
-        patience=config['hp_patience'],
-        min_delta=config['hp_min_delta'],
-        mode='min'
-    )
-
-    stopwatch = Stopwatch()
-    stopwatch.start()
-
-    history = model.fit(
-        catalog.features,
-        catalog.labels,
-        validation_data=(x_valid, y_valid),
-        epochs=config['hp_epochs'],
-        verbose=1,
-        callbacks=[early_stopping]
-    )
-
-    stopwatch.stop()
-
-    model.save(config['saved_model_path'])
-
-    pch.show_history_chart(history, 'accuracy')
-    pch.show_history_chart(history, 'loss')
-
-    lyrics_text = catalog.generate_lyrics_text(
-        model,
-        seed_text=config['seed_text'],
-        word_count=config['words_to_generate']
-    )
-
-    lyrics = LyricsFormatter.format_lyrics(lyrics_text, config['word_group_count'])
-    print(lyrics)
-
-    with open(config['saved_lyrics_path'], 'w') as lyrics_file:
-        lyrics_file.write(lyrics)
-        lyrics_file.close()
+    lyrics_model = PoePoemModel('poe_poem_config.json')
+    lyrics_model.train_model()
 
 
 if __name__ == '__main__':
