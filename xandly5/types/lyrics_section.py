8c93ac5a407803c35f68073ff69db55de9190aea
Andre Oporto <andreoporto@gmail.com>
2021-03-05 18:47:08 -0500

    add StructuredLyricsApi

--- xandly5/types/lyrics_section.py
+++ xandly5/types/lyrics_section.py
@@ -9,7 +9,7 @@ class LyricsSection:
     def __init__(self, section_type: SectionTypeEnum, word_count: int, word_group_count: int, seed_text: str,
                  section_id: Optional[str] = None):
         self.section_id = str(uuid.uuid4()) if section_id is None else section_id
-        self.section_type = section_type
+        self.section_type = section_type if type(section_type) == SectionTypeEnum else SectionTypeEnum(section_type)
         self.word_count = word_count
         self.word_group_count = word_group_count
         self.seed_text = seed_text
--- xandly5/web/lyrics_api.py
+++ xandly5/web/lyrics_api.py
@@ -1,13 +1,16 @@
-
 from flask import Flask, request, make_response, jsonify
+from flask_marshmallow import Marshmallow, fields
+from marshmallow import post_load
 from flask_restful import Resource, Api
 
 from xandly5.service.lyrics_generator import LyricsGenerator
 from xandly5.types.lyrics_model_enum import LyricsModelEnum
 from xandly5.types.validation_error import ValidationError
+from xandly5.types.lyrics_section import LyricsSection
 
 app = Flask(__name__)
 api = Api(app)
+ma = Marshmallow(app)
 
 
 def make_error(status_code: int, message: str):
@@ -17,6 +20,22 @@ def make_error(status_code: int, message: str):
     })
     response.status_code = status_code
     return response
+
+
+class LyricsSectionSchema(ma.Schema):
+    section_id = fields.fields.Str(required=False, allow_none=True, missing=True)
+    section_type = fields.fields.Int()
+    word_count = fields.fields.Int()
+    word_group_count = fields.fields.Int()
+    seed_text = fields.fields.Str()
+
+    # noinspection PyUnusedLocal
+    @post_load
+    def make_section(self, data, **kwargs):
+        return LyricsSection(**data)
+
+
+sections_schema = LyricsSectionSchema(many=True)
 
 
 # noinspection PyMethodMayBeStatic
@@ -39,11 +58,41 @@ class LyricsApi(Resource):
             response.mimetype = "text/plain"
 
             return response
+
+        except ValidationError as ve:
+            return make_error(400, str(ve))
+
+
+# noinspection PyMethodMayBeStatic
+class StructuredLyricsApi(Resource):
+
+    def post(self):
+        try:
+            json_values = request.json
+            model_id: LyricsModelEnum = LyricsModelEnum(json_values['model_id'])
+            independent_sections: bool = json_values['independent_sections']
+            sections = json_values['lyrics_sections']
+            lyrics_sections = sections_schema.load(sections, many=True, unknown='EXCLUDE')
+
+            generator = LyricsGenerator(LyricsModelEnum(model_id))
+            lyrics: str
+
+            if independent_sections:
+                lyrics = generator.generate_lyrics_from_independent_sections(lyrics_sections)
+            else:
+                lyrics = generator.generate_lyrics_from_sections(lyrics_sections)
+
+            response = make_response(lyrics, 200)
+            response.mimetype = "text/plain"
+
+            return response
+
         except ValidationError as ve:
             return make_error(400, str(ve))
 
 
 api.add_resource(LyricsApi, '/lyrics-api')
+api.add_resource(StructuredLyricsApi, '/structured-lyrics-api')
 
 if __name__ == '__main__':
     app.run(debug=False)  # enabling debug causes CUDNN errors
