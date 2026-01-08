749bf48f251362f85b7964fbefc9e5a28958afed
Andre Oporto <andreoporto@gmail.com>
2026-01-08 07:36:22 -0500

    add docker files

--- xandly5/web/lyrics_api.py
+++ xandly5/web/lyrics_api.py
@@ -100,4 +100,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=False)  # enabling debug causes CUDNN errors
+    app.run(debug=False, host='0.0.0.0')  # enabling debug causes CUDNN errors
